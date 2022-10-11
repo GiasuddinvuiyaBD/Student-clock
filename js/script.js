3927dff7e6243edb5023f59566a5ce5b8c87e56d
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2022-10-11 10:21:56 +0600

    second virsion that i face image updating problem virsion 0.1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,43 @@
 // Day : Tuesday
 
 const dateUIElm = document.querySelector('#dateUI');
+const bodyElm = document.querySelector('body');
+
+const allImgElm = [
+	"url('image/b.jpg')",
+	"url('image/default-backround.jpg')",
+	"url('image/img-1.jpg')",
+	"url('image/img-2.jpg')",
+	"url('image/img-3.jpg')",
+	"url('image/img-4.jpg')",
+	"url('image/img-5.jpg')",
+	"url('image/img-6.jpg')",
+	"url('image/img-7.jpg')",
+	"url('image/img-8.jpg')",
+	"url('image/img-9.jpg')",
+	"url('image/img-10.jpg')",
+	"url('image/img-11.jpg')",
+];
+
+// dateUIElm.style.backgroundImage = allImgElm[1];
+function randomNumber(arr)
+{
+	let receveIndexNumber = [];
+
+	for(let i = 1; i <= arr.length; i++)
+	{
+		receveIndexNumber.push(i)
+	}
+
+	console.log(...receveIndexNumber);
+	
+	// console.log(Math.ceil(Math.random() * 3))
+}
+randomNumber(allImgElm)
+
+
+
+// document.body.style.backgroundImage = "url('img_tree.png')";
 
 function dateFun()
 {
